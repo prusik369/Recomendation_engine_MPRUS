d572a4207c63aee1a8f00b744d8f7ad9c01c9130
Mateusz Prus <prusik369@gmail.com>
2025-12-02 12:54:54 +0100

    creating_engine_recomendation_model_creation

--- Utils_app.py
+++ Utils_app.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 import pandas as pd
+import numpy as np
 
 
 def compute_weighted_rating(df, rating_col="rating", min_votes=5):
@@ -63,3 +64,44 @@ def filter_by_time(df, option):
 
 
 
+
+def recommend_existing_users(model, train_matrix, top_n=5, user_ids=None):
+    """
+    Generate top-N recommendations for existing users using a trained SVD model.
+
+    Args:
+        model: Trained TruncatedSVD model.
+        train_matrix (pd.DataFrame): User x Product rating matrix.
+        top_n (int): Number of top recommendations per user.
+        user_ids (list or None): List of user_ids to generate recommendations for. 
+                                 If None, generate for all users in train_matrix.
+
+    Returns:
+        dict: {user_id: list of top-N product_ids}
+    """
+    # If user_ids not provided, take all users
+    if user_ids is None:
+        user_ids = train_matrix.index.tolist()
+    
+    # Transform train_matrix with SVD
+    svd_matrix = model.transform(train_matrix)
+    approx_matrix = pd.DataFrame(
+        np.dot(svd_matrix, model.components_),
+        index=train_matrix.index,
+        columns=train_matrix.columns
+    )
+    
+    recommendations = {}
+    for uid in user_ids:
+        if uid not in train_matrix.index:
+            continue  # skip users not in training data
+        # Sort predicted ratings for user in descending order
+        user_preds = approx_matrix.loc[uid]
+        # Remove already rated items
+        rated_items = train_matrix.loc[uid][train_matrix.loc[uid] > 0].index
+        user_preds = user_preds.drop(rated_items)
+        # Take top-N
+        top_products = user_preds.sort_values(ascending=False).head(top_n).index.tolist()
+        recommendations[uid] = top_products
+
+    return recommendations
